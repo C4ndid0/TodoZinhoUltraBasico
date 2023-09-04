2bc70609b1852a8f87b00513c49e861f1092c87e
Lucas Candido <avnserver@gmail.com>
2023-09-04 11:32:39 -0300

    -

---
+++ src/main/java/br/com/candidoLucas/todozinho/TodozinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.candidoLucas.todozinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodozinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodozinhoApplication.class, args);
+	}
+
+}
