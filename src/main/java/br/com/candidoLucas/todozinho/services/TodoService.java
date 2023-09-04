0bff473f0f5b49807d2f3fc7383ea445e2e2dfbb
Lucas Candido <avnserver@gmail.com>
2023-09-04 11:31:14 -0300

    feat: Todo Service

---
+++ src/main/java/br/com/candidoLucas/todozinho/services/TodoService.java
@@ -0,0 +1,44 @@
+package br.com.candidoLucas.todozinho.services;
+
+import br.com.candidoLucas.todozinho.entity.Todo;
+import br.com.candidoLucas.todozinho.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private  TodoRepository todoRepository;
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort
+                .by("priority")
+                .descending()
+                .and(Sort.by("name").ascending());
+        return todoRepository.findAll(sort);
+
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+      return list();
+    }
+
+
+}
