f570fcefbb44f04e64d63bac7e366f3479020a92
Lucas Candido <avnserver@gmail.com>
2023-09-04 11:30:36 -0300

    feat: Todo Controller

---
+++ src/main/java/br/com/candidoLucas/todozinho/controller/TodoController.java
@@ -0,0 +1,41 @@
+package br.com.candidoLucas.todozinho.controller;
+
+import br.com.candidoLucas.todozinho.entity.Todo;
+import br.com.candidoLucas.todozinho.services.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<Todo> create(@RequestBody @Valid Todo todo){
+        return todoService.create(todo);
+
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
